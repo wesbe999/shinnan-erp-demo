4aadbfe1e008317a5e08391918a7b2b8ac00017d
wesbe999 <wesbe@msn.com>
2026-05-17 04:45:55 +0800

    fix: login card down 90px total

--- app/routes/employee_auth.py
+++ app/routes/employee_auth.py
@@ -172,7 +172,7 @@ def _employee_login_page(error: str = "", next_url: str = "/app"):
       display: flex;
       align-items: center;
       justify-content: center;
-      padding-top: 70px;
+      padding-top: 90px;
       font-family: "Noto Sans TC", "Microsoft JhengHei", Arial, sans-serif;
       overflow: hidden;
       background-color: #04120e;
