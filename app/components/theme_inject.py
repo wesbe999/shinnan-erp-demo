4cf24cda9a4dd8e814309bb4a359ea6f14cf72e5
wesbe999 <wesbe@msn.com>
2026-05-24 15:05:44 +0800

    fix: bump theme.css to v3, fix margin/border-radius on navy header

--- app/components/theme_inject.py
+++ app/components/theme_inject.py
@@ -21,7 +21,7 @@ def apply_theme(html: str, request: Request) -> str:
     for ver in ["xn_v2", "xn_v1", "cl_header_v1"]:
         marker = f'href="/static/web_title_unified.css?v={ver}">'
         if marker in html:
-            theme_css = f'<link rel="stylesheet" href="/static/themes/{theme}/theme.css?v={ver}">'
+            theme_css = f'<link rel="stylesheet" href="/static/themes/{theme}/theme.css?v=xn_v3">'
             html = html.replace(marker, marker + f'\n  {theme_css}', 1)
             break
     return html
